a36b3da35e4bee9e10a353e98e5467bc338b39a9
mimo <mouedh.souabni@etu.univ-grenoble-alpes.com>
2025-03-19 07:15:20 +0100

    Wrote subroutine for finding index of minimum and started node tree making subroutine

--- phuf.pl
+++ phuf.pl
@@ -3,6 +3,7 @@
 
 # Subroutine for reading in the file and determining the weights of each word and adding them to the tree structures
 %char_weight = ();
+
 sub rd_weights {
   open(FILE, '<', @_[0]) or die "Cannot open file $filename for reading: $!";
   # @_[0] is the first element in the list of arguments
@@ -33,6 +34,30 @@ rd_weights('LICENSE');
 # ----------------------------------------
 
 # Subroutine defined for the tree object that brings the nodes together to create de encoding tree
+sub min {
+  my @vals = @_[0];
+  my $m=0;
+  my $i=0;
+  foreach $x ( @vals ) {
+    if ($x<$m) {
+      $m=$i;
+    }
+    $i++;
+  }
+  $m;
+}
+
+sub create_binary_tree {
+  my @vals = values %char_weight;
+  my @ks = keys %char_weight;
+  my @children = ();
+  while ( $ks != 0 ) {
+    my $min_index = min(@vals);
+    # add it to the children array
+    # delete it from the ks array
+    # do that a 2nd time
+  }
+}
 # Subroutine for decompressing, parses the header to decode the file
 # Add a custom pseudo-EOF signature 8 bits that won't be used in the encoding
 # Put the tree data structure in format as a header
